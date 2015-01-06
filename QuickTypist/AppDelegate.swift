a1771c650bd86e6d5fe7949e8facfa0932d9b555
Mikael Hallendal <hallski@hallski.org>
2015-01-06 21:27:25 +0100

    Initial Commit

---
+++ QuickTypist/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  QuickTypist
+//
+//  Created by Mikael Hallendal on 06/01/15.
+//  Copyright (c) 2015 Mikael Hallendal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
